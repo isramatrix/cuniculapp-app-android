e8b88db2f239ce69389f1dcf4807f2caa21b3923
isramatrix <israel.concoma@gmail.com>
2020-03-29 20:49:37 +0200

    Se implementa la navegabilidad entre fragmentos

--- CuniculappAndroid/app/src/main/java/upv/cuniculappteam/cuniculapp/activity/main/MainActivity.java
+++ CuniculappAndroid/app/src/main/java/upv/cuniculappteam/cuniculapp/activity/main/MainActivity.java
@@ -1,5 +1,6 @@
 package upv.cuniculappteam.cuniculapp.activity.main;
 
+import androidx.annotation.IdRes;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -10,6 +11,9 @@ import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import upv.cuniculappteam.cuniculapp.R;
 import upv.cuniculappteam.cuniculapp.activity.calendar.CalendarFragment;
 import upv.cuniculappteam.cuniculapp.activity.cycles.CyclesFragment;
@@ -19,7 +23,17 @@ import upv.cuniculappteam.cuniculapp.activity.settings.SettingsFragment;
 public class MainActivity extends AppCompatActivity implements
         BottomNavigationView.OnNavigationItemSelectedListener
 {
-    private static final Fragment mainFragment = new CyclesFragment();
+    private static final Map<Integer, Fragment> fragments = new HashMap<>();
+    static {
+        fragments.put(R.id.main_calendar, new CalendarFragment());
+        fragments.put(R.id.main_cycles, new CyclesFragment());
+        fragments.put(R.id.main_labor, new LaborFragment());
+        fragments.put(R.id.main_settings, new SettingsFragment());
+    }
+
+    private static final @IdRes int mainFragment = R.id.main_cycles;
+
+    private Fragment activeFragment;
 
     private BottomNavigationView menu;
 
@@ -29,30 +43,46 @@ public class MainActivity extends AppCompatActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initializeFragment();
+
         this.menu = findViewById(R.id.main_menu);
         this.menu.setOnNavigationItemSelectedListener(this);
+        this.menu.setSelectedItemId(mainFragment);
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item)
     {
-        switch (item.getItemId())
-        {
-            case R.id.main_calendar: switchFragment(new CalendarFragment()); break;
-            case R.id.main_labor: switchFragment(new LaborFragment()); break;
-            case R.id.main_cycles: switchFragment(new CyclesFragment()); break;
-            case R.id.main_settings: switchFragment(new SettingsFragment()); break;
-            default: break;
-        }
+        Fragment fragment;
+        if ((fragment = fragments.get(item.getItemId())) != null)
+            switchFragment(fragment);
 
         return true;
     }
 
-    private void switchFragment(Fragment fragment)
+    private void switchFragment(@NonNull Fragment fragment)
     {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.main_fragment, fragment);
-        //if (!fragment.equals(mainFragment)) transaction.addToBackStack(null);
+
+        if (activeFragment != null)
+            transaction.hide(activeFragment);
+        transaction.show(fragment);
+        transaction.commit();
+
+        activeFragment = fragment;
+    }
+
+    private void initializeFragment()
+    {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        Fragment fragment;
+
+        for (Integer id : fragments.keySet())
+        {
+            if ((fragment = fragments.get(id)) != null)
+                transaction.add(R.id.main_fragment, fragment).hide(fragment);
+        }
+
         transaction.commit();
     }
 }
