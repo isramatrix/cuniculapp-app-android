9271ccf767633eb3ecd54773ab6d0918c8000dc9
luiserh1 <54946908+luiserh1@users.noreply.github.com>
2020-04-26 23:30:18 +0200

    ViewModel Partial Implementation

--- CuniculappAndroid/app/src/main/java/upv/cuniculappteam/cuniculapp/viewmodel/LaborViewModel.java
+++ CuniculappAndroid/app/src/main/java/upv/cuniculappteam/cuniculapp/viewmodel/LaborViewModel.java
@@ -7,6 +7,7 @@ import com.google.android.gms.tasks.Tasks;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 
 import upv.cuniculappteam.cuniculapp.logic.firebase.Firebase;
@@ -21,19 +22,69 @@ public class LaborViewModel extends ViewModel
         return Database.fetchAll(Labor.class);
     }
 
-    public Task<List<Labor>> addLabor(Labor labor)
+    public Task<Void> addLabor(Labor labor)
     {
-        return null;// Database.add(labor.getId(), labor);
+        return Firebase.Database.add(Labor.class, labor);
+    }
+
+    public Task<List<Labor>> getArchivedLabors()
+    {
+        return Firebase.Database.fetchWhere("state", Labor.State.ARCHIVED, Labor.class);
+    }
+
+    public Task<List<Labor>> getUnarchivedLabors()
+    {
+        Task<List<Labor>> taskDone =
+                Firebase.Database.fetchWhere("state", Labor.State.DONE, Labor.class);
+        Task<List<Labor>> taskTodo =
+                Firebase.Database.fetchWhere("state", Labor.State.TO_DO, Labor.class);
+        List<Labor> taskResult =  new ArrayList<Labor>();
+        /*
+        return Tasks.whenAllSuccess(taskTodo, taskDone).continueWith((Task<List<Labor>> task) ->
+            {
+                List<List<Labor>> lRes = task.getResult();
+                for (List<Labor> t : lRes) taskResult.addAll(t);
+                return taskResult;
+            }
+        );*/
+        return taskDone;
     }
 
     public Task<Void> completeLabor(Labor labor)
     {
-        return null;
+        if (!labor.isFinished()) labor.setFinishedDate(new Date());
+        return Database.update(labor, Labor.class);
+    }
+
+    public Task<Void> doLabor(Labor labor)
+    {
+        if (labor.getState() == Labor.State.TO_DO) labor.setState(Labor.State.DONE);
+        return Database.update(labor, Labor.class);
+    }
+
+    public Task<Void> UndoLabor(Labor labor)
+    {
+        if (labor.getState() == Labor.State.DONE) labor.setState(Labor.State.TO_DO);
+        return Database.update(labor, Labor.class);
+    }
+
+    public Task<Void> archiveLabor(Labor labor)
+    {
+        if (labor.getState() == Labor.State.DONE) labor.setState(Labor.State.ARCHIVED);
+        return Database.update(labor, Labor.class);
+    }
+
+    public Task<Void> unArchiveLabor(Labor labor)
+    {
+        if (labor.getState() == Labor.State.ARCHIVED) labor.setState(Labor.State.DONE);
+        return Database.update(labor, Labor.class);
     }
 
     public Task<List<Labor>> deleteLabors(Collection<Labor> labors)
     {
-        return Tasks.call(ArrayList::new);
+        for (Labor labor : labors)
+            Firebase.Database.delete(Labor.class, labor);
+        return null;
     }
-    Database.
+
 }
